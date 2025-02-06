f17ce3340393e52fe54743eafe26389d238131ff
Kristin <kristindyulgeryan@abv.bg>
2025-02-06 11:52:30 +0200

    Setup node modules, debugger, express

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('It works');
+}).listen(3000, ()=> console.log('Server is listening on http://localhost:3000...'));
