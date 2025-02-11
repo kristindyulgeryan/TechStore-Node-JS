a0085ac3bd6fe14b69709910d99f71cd1f3d6fe0
Kristin <kristindyulgeryan@abv.bg>
2025-02-11 11:50:24 +0200

    Create DB device  model

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -6,7 +6,7 @@ export const generateToken= async (user) =>{
     const payload = {
         id: user.id,
         email: user.email,
-        username: user.username,
+        name: user.name,
     };
     const token = await jsonwebtoken.sign(payload, JWT_SECRET, {expiresIn: '2h'});
     return token;
