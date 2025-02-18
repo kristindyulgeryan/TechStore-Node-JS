8603526ed2acfcf702968703761b9d4a39448419
Kristin <kristindyulgeryan@abv.bg>
2025-02-18 20:59:32 +0200

    Fix login and register navigation mistake

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,55 +4,56 @@ import { AUTH_COOKIE_NAME } from "../config.js";
 import { isAuth, isGuest } from "../middlewares/authMiddleware.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
 
-
 const authController = Router();
 
-
-authController.get('/login', isGuest, (req, res)=>{
-    res.render('auth/login')
+authController.get("/login", isGuest, (req, res) => {
+  res.render("auth/login");
 });
 
-authController.post('/login', isGuest, async (req, res)=>{
-    const { email, password } = req.body;
+authController.post("/login", isGuest, async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-        const token = await authService.login( email, password );
+  try {
+    const token = await authService.login({ email, password });
 
-    res.cookie(AUTH_COOKIE_NAME, token, {httpOnly: true});
-    res.redirect('/')
-    } catch (err) {
-        res.render('auth/login', {
-            error: getErrorMessage(err), 
-            user: { email}})
-    }
-    
+    res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+    res.redirect("/");
+  } catch (err) {
+    res.render("auth/login", {
+      error: getErrorMessage(err),
+      name: { email },
+    });
+  }
 });
 
-
-authController.get('/register', isGuest, (req, res)=>{
-    res.render('auth/register')
+authController.get("/register", isGuest, (req, res) => {
+  res.render("auth/register");
 });
 
-authController.post('/register',isGuest, async (req, res)=>{
-    const userData = req.body;
-    console.log(userData);
+authController.post("/register", isGuest, async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
 
-try {
-    const token =  await authService.register(userData);
+  try {
+    const token = await authService.register({
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
 
-    res.cookie(AUTH_COOKIE_NAME, token, {httpOnly: true})
-    res.redirect('/')
-} catch (err) {
-    res.render('auth/register', {error: getErrorMessage(err), user: userData});
-}
- 
-
-    
+    res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+    res.redirect("/");
+  } catch (err) {
+    res.render("auth/register", {
+      error: getErrorMessage(err),
+      user: { name, email },
+    });
+  }
 });
 
-authController.get('/logout', isAuth, (req, res)=> {
-    res.clearCookie(AUTH_COOKIE_NAME);
-    res.redirect('/');
-})
+authController.get("/logout", isAuth, (req, res) => {
+  res.clearCookie(AUTH_COOKIE_NAME);
+  res.redirect("/");
+});
 
 export default authController;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,47 +1,45 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import User from "../models/User.js";
-import { generateToken } from '../utils/authUtils.js';
+import { generateToken } from "../utils/authUtils.js";
 
+export const login = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid user or email");
+  }
 
-export const login = async ( email, password ) => {
-   
-    const user = await User.findOne({ email });
-    if(!user){
-        throw new Error("Invalid user or email"); 
-    };
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Invalid user or email");
+  }
 
-    const isValid = await bcrypt.compare( password, user.password);
-    if(!isValid){
-        throw new Error("Invalid user or email");   
-    };
-
-    const token = generateToken(user);
-    return token;
+  const token = generateToken(user);
+  return token;
 };
 
-export const register = async (userData)=> {
-    if(userData.password !== userData.confirmPassword){
-        throw new Error("Password missmatch!");
-        
-    }
+export const register = async (userData) => {
+  if (userData.password !== userData.confirmPassword) {
+    throw new Error("Password missmatch!");
+  }
 
-    const user = await User.findOne({ email: userData.email }).select({_id: true});
-   
-    if(user){
-        throw new Error('User already exists')
-    };
-   
-    const createdUser = await User.create(userData);
+  const user = await User.findOne({ email: userData.email }).select({
+    _id: true,
+  });
 
-    const token = generateToken(createdUser);
+  if (user) {
+    throw new Error("User already exists");
+  }
 
-    return token;
+  const createdUser = await User.create(userData);
+
+  const token = generateToken(createdUser);
+
+  return token;
 };
-
 
 const authService = {
-    register, 
-    login
+  login,
+  register,
 };
 
 export default authService;
